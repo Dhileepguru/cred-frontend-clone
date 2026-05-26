361a415549e65232264767f1af3c5029dabc8110
Dhileepguru <dhileepguru497@gmail.com>
2026-05-26 15:25:43 +0530

    Inserted trust Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'; // நம்ம ஹீரோ ஃபைல் இம்போர்ட்
+import Trust from './components/Trust';
 
 function App() {
   return (
     <div className="bg-black text-white min-h-screen antialiased">
       <Navbar />
       <Hero />
+      <Trust />
     </div>
   );
 }
